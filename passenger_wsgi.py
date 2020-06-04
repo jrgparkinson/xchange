69f6fd21c562542a8d5649ad9ff6a23e10776f19
jparkinson <jamie.parkinson@gmail.com>
2020-06-04 22:47:21 +0100

    Use my app in passenger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,10 +4,11 @@ import sys
 
 sys.path.insert(0, os.path.dirname(__file__))
 
+from xchange.wsgi import application
 
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works!\n'
-    version = 'Python %s\n' % sys.version.split()[0]
-    response = '\n'.join([message, version])
-    return [response.encode()]
+# def application(environ, start_response):
+#     start_response('200 OK', [('Content-Type', 'text/plain')])
+#     message = 'It works!\n'
+#     version = 'Python %s\n' % sys.version.split()[0]
+#     response = '\n'.join([message, version])
+#     return [response.encode()]
